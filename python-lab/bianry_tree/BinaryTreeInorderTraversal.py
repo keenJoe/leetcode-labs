65d2fa09f8d7b5b82171173b1b90896cbb6073d5
XuQidong <qidongxuca@gmail.com>
2025-01-11 09:51:09 +0800

    94. Binary Tree Inorder Traversal using recursion

--- python-lab/bianry_tree/BinaryTreeInorderTraversal.py
+++ python-lab/bianry_tree/BinaryTreeInorderTraversal.py
@@ -11,6 +11,21 @@ class TreeNode:
         self.right = right
 
 
+# 中序遍历：左子树 -> 根节点 -> 右子树
 class Solution:
+    # 递归
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        self.dfs_inorder(root, res)
+        return res
+
+    def dfs_inorder(self, root: Optional[TreeNode], res: List[int]) -> List[int]:
+        if root:
+            self.dfs_inorder(root.left, res)
+            res.append(root.val)
+            self.dfs_inorder(root.right, res)
+        return res
+
+    # 迭代
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         pass
