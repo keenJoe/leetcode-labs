9de7a2a7e3faa2b10ba82c1f9c997e06565c52a7
XuQidong <qidongxuca@gmail.com>
2025-01-12 11:10:19 +0800

    后序遍历

--- python-lab/bianry_tree/BinaryTreePostorderTraversal.py
+++ python-lab/bianry_tree/BinaryTreePostorderTraversal.py
@@ -28,15 +28,69 @@ class Solution:
     # 迭代
     def postorderTraversal_using_stack(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-
-        # todo
+        if not root:
+            return res
+        
+        stack = []
+        stack.append(root)
+        while stack:
+            # 获取栈顶元素
+            node = stack[-1]
+            if not node.left and not node.right:
+                res.append(node.val)
+                stack.pop()
+            else:
+                if node.right:
+                    stack.append(node.right)
+                    node.right = None
+                if node.left:
+                    stack.append(node.left)
+                    node.left = None
 
         return res
     
     # Morris遍历
     def postorderTraversal_using_morris(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-
-        # todo
-
+        if not root:
+            return res
+        
+        cur = root
+        while cur:
+            if not cur.left:
+                cur = cur.right
+            else:
+                pre = cur.left
+                while pre.right and pre.right != cur:
+                    pre = pre.right
+                
+                if not pre.right:
+                    pre.right = cur
+                    cur = cur.left
+                else:
+                    pre.right = None
+                    # 添加反转后的路径
+                    self.addPath(cur.left, res)
+                    cur = cur.right
+        
+        # 最后添加整个右边界
+        self.addPath(root, res)
         return res
+    
+    def addPath(self, node: TreeNode, res: List[int]):
+        tail = self.reverseNode(node)
+        cur = tail
+        while cur:
+            res.append(cur.val)
+            cur = cur.right
+        self.reverseNode(tail)  # 恢复原来的顺序
+    
+    def reverseNode(self, node: TreeNode) -> TreeNode:
+        pre = None
+        cur = node
+        while cur:
+            next = cur.right
+            cur.right = pre
+            pre = cur
+            cur = next
+        return pre
