a8407d1adfcd7f05526535dd54c113677613d139
qidongxu <qidongxuca@gmail.com>
2025-01-13 17:37:42 +0800

    same tree iteration

--- python-lab/bianry_tree/SameTree.py
+++ python-lab/bianry_tree/SameTree.py
@@ -23,4 +23,30 @@ class Solution:
 
     # 迭代
     def isSameTree_iter(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        pass
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        
+        p_stack = [p]
+        q_stack = [q]
+        
+        while p_stack and q_stack:
+            p_node = p_stack.pop()
+            q_node = q_stack.pop()
+            if p_node.val != q_node.val:
+                return False
+            
+            if p_node.left and q_node.left:
+                p_stack.append(p_node.left)
+                q_stack.append(q_node.left)
+            elif p_node.left or q_node.left:
+                return False
+            
+            if p_node.right and q_node.right:
+                p_stack.append(p_node.right)
+                q_stack.append(q_node.right)
+            elif p_node.right or q_node.right:
+                return False
+            
+        return True
