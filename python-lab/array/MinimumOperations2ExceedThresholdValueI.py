5832c3b59917a9f97b27561f10a16b27f56af66e
XuQidong <qidongxuca@gmail.com>
2025-01-15 23:41:56 +0800

    20250115

--- python-lab/array/MinimumOperations2ExceedThresholdValueI.py
+++ python-lab/array/MinimumOperations2ExceedThresholdValueI.py
@@ -5,4 +5,8 @@ from typing import List
 
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        pass
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] < k:
+                count += 1
+        return count
