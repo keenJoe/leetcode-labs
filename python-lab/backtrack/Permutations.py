09da8f9ae60b9be6ab26ac2cc58ea8439fce7f54
qidongxu <qidongxuca@gmail.com>
2025-02-06 11:14:01 +0800

    交换法

--- python-lab/backtrack/Permutations.py
+++ python-lab/backtrack/Permutations.py
@@ -34,6 +34,55 @@ class Solution:
         backtrack([])
         return result
     
+    
+    # 使用visited数组来判断元素是否已经用过
+    # 但是也没有优化太多
+    def permute_2(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(path, visited):
+            if len(path) == len(nums):
+                yield path[:]
+                return
+            
+            for i in range(len(nums)):
+                if visited[i]:  # 使用O(1)时间复杂度检查是否访问过
+                    continue
+                visited[i] = True
+                path.append(nums[i])
+                yield from backtrack(path, visited)
+                path.pop()
+                visited[i] = False
+        
+        return list(backtrack([], [False] * len(nums)))
+    
+    # 交换法
+    def permute_3(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first=0):
+            print('***********')
+            print('nums', nums)
+            print('first', first)
+            if first == n:  
+                res.append(nums[:])
+                return
+            
+            for i in range(first, n):
+                print('i', i)
+                # 交换，将当前数字放到first位置
+                nums[first], nums[i] = nums[i], nums[first]
+                # 递归填下一个位置
+                backtrack(first + 1)
+                # 回溯，恢复原状
+                nums[first], nums[i] = nums[i], nums[first]
+                print('===========')
+                print('back nums', nums)
+                print('back first', first)
+                print('back i', i)
+                print('===========')
+        
+        n = len(nums)
+        res = []
+        backtrack()
+        return res
+    
 if __name__ == "__main__":
     nums = [1,2,3]
-    print(Solution().permute(nums))
+    print(Solution().permute_3(nums))
