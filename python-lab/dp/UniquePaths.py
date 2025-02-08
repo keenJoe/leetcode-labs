fc9079ce083a180b6e60694757e71d4a1f032957
qidongxu <qidongxuca@gmail.com>
2025-02-08 13:14:19 +0800

    优化

--- python-lab/dp/UniquePaths.py
+++ python-lab/dp/UniquePaths.py
@@ -46,6 +46,17 @@ class Solution:
             for j in range(1, n):
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         return dp[m - 1][n - 1]
+    
+    # 使用动态规划，优化空间复杂度
+    # 时间复杂度：O(m * n)
+    # 空间复杂度：O(n)
+    def uniquePaths_2(self, m: int, n: int) -> int:
+        dp = [1] * n
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] = dp[j] + dp[j - 1]
+        return dp[n - 1]
 
 if __name__ == "__main__":
     m = 3
