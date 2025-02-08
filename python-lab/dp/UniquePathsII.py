acc2c447c211373ad79980482a511fa271f2cf15
qidongxu <qidongxuca@gmail.com>
2025-02-08 13:32:00 +0800

    63. Unique Paths II 带有障碍物

---
+++ python-lab/dp/UniquePathsII.py
@@ -0,0 +1,67 @@
+# 63. Unique Paths II
+
+'''
+    这道题和Unique Paths非常相似，唯一的区别是，这道题中，网格中可能存在障碍物。
+    障碍物用1表示，空地用0表示。
+    机器人从左上角出发，只能向下或向右移动，不能向上或向左移动。
+    问机器人从左上角到右下角有多少条不同的路径。
+'''
+
+from typing import List
+
+
+class Solution:
+    # 使用递归
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        
+        if obstacleGrid[0][0] == 1:
+            return 0
+        if obstacleGrid[m-1][n-1] == 1:
+            return 0
+        
+        def uniquePathsWithObstacles_recursion(x: int, y: int) -> int:
+            if x == m - 1 and y == n - 1:
+                return 1
+            if x >= m or y >= n or obstacleGrid[x][y] == 1:
+                return 0
+            return uniquePathsWithObstacles_recursion(x + 1, y) + uniquePathsWithObstacles_recursion(x, y + 1)
+        return uniquePathsWithObstacles_recursion(0, 0)
+    
+    # 使用动态规划
+    def uniquePathsWithObstacles_1(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        
+        if obstacleGrid[0][0] == 1:
+            return 0
+        if obstacleGrid[m-1][n-1] == 1:
+            return 0
+        
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+        
+        for i in range(1, m):
+            if obstacleGrid[i][0] == 1:
+                break
+            dp[i][0] = 1
+        
+        for j in range(1, n):
+            if obstacleGrid[0][j] == 1:
+                break
+            dp[0][j] = 1
+            
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+                
+        return dp[m - 1][n - 1]
+    
+    
+    
+if __name__ == "__main__":
+    obstacleGrid = [[0,0],[0,1]]
+    print(Solution().uniquePathsWithObstacles(obstacleGrid))
+    print(Solution().uniquePathsWithObstacles_1(obstacleGrid))
