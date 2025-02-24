21ab81d46661844b2dd3f2460fedfc7194efc295
qidongxu <qidongxuca@gmail.com>
2025-02-24 22:07:17 +0800

    1656. Design an Ordered Stream

---
+++ python-lab/dp/2209.MinimumWhiteTilesAfterCoveringWithCarpets.py
@@ -0,0 +1,15 @@
+# 2209. Minimum White Tiles After Covering With Carpets
+
+from typing import List
+
+
+class Solution:
+    def minimumWhiteTiles(self, floor: str, numCarpets: int, carpetLen: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    floor = "11111"
+    numCarpets = 2
+    carpetLen = 3
+    print(Solution().minimumWhiteTiles(floor, numCarpets, carpetLen))
