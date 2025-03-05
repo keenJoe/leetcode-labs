31e1aa3c78f2d2fb1379c032501ddc6b6bdeac80
qidongxu <qidongxuca@gmail.com>
2025-03-05 10:13:26 +0800

    理解旋转的有序数组中使用二分查找

--- python-lab/array/81.SearchInRotatedSortedArrayII.py
+++ python-lab/array/81.SearchInRotatedSortedArrayII.py
@@ -1,10 +1,19 @@
 # 81. Search in Rotated Sorted Array II
 
 '''
-    1、先找到旋转点；
-
-    2、然后判断target在旋转点的左边还是右边；
-    3、最后在左边或右边进行二分查找；
+    1、先判断mid和target的大小关系；
+    2、如果mid和target相等，则返回True；
+    3、如果mid和target不等，处理边界值；
+        （1）如果mid和target不等，且nums[l] <= nums[mid]，则说明左半部分有序；
+        （2）如果nums[l] <= target and target < nums[mid]，则说明target在左半部分，则将右边界缩小到mid-1；
+        （3）否则，说明target在右半部分，则将左边界缩小到mid+1；
+        
+        （4）如果mid和target不等，且nums[l] > nums[mid]，则说明右半部分有序；
+        （5）如果nums[mid] < target and target <= nums[n - 1]，则说明target在右半部分，则将左边界缩小到mid+1；
+        （6）否则，说明target在左半部分，则将右边界缩小到mid-1；
+    10、最后返回False；
+    
+    在3中确定哪一部分有序非常重要。
 '''
 
 from typing import List
@@ -12,6 +21,39 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
+        if not nums:
+            return False
+        
+        n = len(nums)
+        if n == 1:
+            return nums[0] == target
+        
+        l, r = 0, n - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return True
+            
+            # 处理左中右三个位置的值都相同的特殊情况
+            if nums[l] == nums[mid] and nums[mid] == nums[r]:
+                l += 1
+                r -= 1
+            elif nums[l] <= nums[mid]:
+                # 目标值在左半有序部分，这种情况下，左侧一定有序
+                if nums[l] <= target and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target and target <= nums[n - 1]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        
+        return False
+    
+    
+    def search_1(self, nums: List[int], target: int) -> bool:
         if not nums:
             return False
             
