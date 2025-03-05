87b2a47d1909f4a6eeb880e94c48cc1a367c0eec
qidongxu <qidongxuca@gmail.com>
2025-03-05 12:45:52 +0800

    复习59. Spiral Matrix II

--- python-lab/array/SpiralMatrixII.py
+++ python-lab/array/SpiralMatrixII.py
@@ -43,6 +43,39 @@ class Solution:
             left += 1
 
         return matrix
+    
+    
+    # def generateMatrix(self, n: int) -> List[List[int]]:
+    #     matrix = [[0] * n for _ in range(n)]
+    #     top, bottom, left, right = 0, n - 1, 0, n - 1
+    #     num = 1
+        
+    #     while top <= bottom and left <= right:
+    #         # 从左到右填充上边，top不变，增加
+    #         for i in range(left, right + 1):
+    #             matrix[top][i] = num
+    #             num += 1
+    #         top += 1
+        
+    #         # 从上到下填充右边，right不变，遍历top到bottom
+    #         for i in range(top, bottom + 1):
+    #             matrix[i][right] = num
+    #             num += 1
+    #         right -= 1
+            
+    #         # 从右到左填充下边，bottom不变，遍历right到left
+    #         for i in range(right, left -1 ,-1):
+    #             matrix[bottom][i] = num
+    #             num += 1
+    #         bottom -= 1
+            
+    #         # 从下到上填充左边，left不变，遍历bottom到top
+    #         for i in range(bottom, top - 1, -1):
+    #             matrix[i][left] = num
+    #             num += 1
+    #         left += 1
+        
+    #     return matrix
 
 
 if __name__ == "__main__":
