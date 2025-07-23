3ced69e96e487fac3d45f88714ea1dea30b18143
qidongxu <qidongxuca@gmail.com>
2025-07-23 11:46:45 +0800

    复习增加注释

--- python-lab/twopointers/26.RemoveDuplicatesFromSortedArray.py
+++ python-lab/twopointers/26.RemoveDuplicatesFromSortedArray.py
@@ -21,15 +21,22 @@ class Solution:
         i = 0
         j = 1
         while j < len(nums):
+            # 如果nums[i] == nums[j]，则j++
             if nums[i] == nums[j]:
                 j += 1
+            # 如果nums[i] != nums[j]，则i++，nums[i] = nums[j]，j++
             else:
+                # 如果 i + 1 == j，相当于 i 和 j 此时指向同一个元素，赋值只是覆盖
+                # 如果 i + 1 != j，相当于 i 和 j 此时指向不同的元素，赋值相当于覆盖了重复的元素
                 i += 1
-                nums[i] = nums[j]
+                if i != j:
+                    # 将nums[j]赋值给nums[i]，相当于覆盖了重复的元素
+                    nums[i] = nums[j]
                 j += 1
+        # 返回 i + 1 是因为 i 是索引，需要 +1 才是长度
         return i + 1
     
 if __name__ == "__main__":
     solution = Solution()
-    nums = [1, 1, 2]
+    nums = [1, 2,3,3,4,5,5,6]
     print(solution.removeDuplicates(nums))
