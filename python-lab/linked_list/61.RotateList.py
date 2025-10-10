5e3b2cc03c28dc848d4c3af523b7a669873f3926
qidongxu <qidongxuca@gmail.com>
2025-10-10 18:47:20 +0800

    提交代码

--- python-lab/linked_list/61.RotateList.py
+++ python-lab/linked_list/61.RotateList.py
@@ -56,6 +56,31 @@ class Solution:
         
         return new_head
 
+    def rotateRightUsingRing(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+
+        # 计算链表长度
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+        
+        # 构成环
+        tail.next = head
+
+        k = k % length
+        steps_to_new_head = length - k
+        new_tail = head
+        for _ in range(steps_to_new_head - 1):
+            new_tail = new_tail.next
+        
+        new_head = new_tail.next
+        new_tail.next = None
+
+        return new_head
+
 
 if __name__ == "__main__":
     solution = Solution()
