463c73e3d79e1fb89bfed652da018477b173ee7c
qidong.xu <qidong.xu@tcredit.com>
2025-10-24 22:38:40 +0800

    update

--- python-lab/linked_list/148.SortList.py
+++ python-lab/linked_list/148.SortList.py
@@ -1,1 +1,28 @@
+<<<<<<< HEAD
 # 148. Sort List
+=======
+# 148. Sort List
+
+''''''
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+if __name__ == "__main__":
+    solution = Solution()
+    head = ListNode(4)
+    head.next = ListNode(2)
+    head.next.next = ListNode(1)
+    head.next.next.next = ListNode(3)
+    print(solution.sortList(head))
+>>>>>>> e2ded62 (update)
--- python-lab/linked_list/160.IntersectionOfTwoLinkedLists.py
+++ python-lab/linked_list/160.IntersectionOfTwoLinkedLists.py
@@ -1,6 +1,7 @@
 # 160. Intersection of Two Linked Lists
 
 '''
+<<<<<<< HEAD
 方案一：
     1、分别计算两个链表的长度；
     2、计算两个链表长度的差值
@@ -17,12 +18,26 @@
 from typing import Optional
 
 # Definition for singly-linked list.
+=======
+1、分别计算两个链表的长度
+2、计算两个链表的差值
+3、然后让长度较长的链表先走差值步
+4、然后两个链表同时走，如果两个链表的节点相同，则返回节点
+5、如果两个链表没有相同节点，则返回None
+'''
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+>>>>>>> e2ded62 (update)
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
 class Solution:
+<<<<<<< HEAD
     def getIntersectionNode1(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         if headA is None or headB is None:
             return None
@@ -74,3 +89,44 @@ if __name__ == '__main__':
     headA = ListNode(4, ListNode(1, ListNode(8, ListNode(4, ListNode(5)))))
     headB = ListNode(5, ListNode(6, ListNode(1, ListNode(8, ListNode(4, ListNode(5))))))
     print(solution.getIntersectionNode2(headA, headB).val)
+=======
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if headA is None or headB is None:
+            return None
+        
+        lenA = self.getLength(headA)
+        lenB = self.getLength(headB)
+        diff = abs(lenA - lenB)
+        if lenA > lenB:
+            headA = self.moveHead(headA, diff)
+        else:
+            pass
+
+
+        return None
+    
+    def getLength(self, head: ListNode) -> int:
+        if head is None:
+            return 0
+        
+        length = 0
+        while head:
+            length += 1
+            head = head.next
+        return length
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    headA = ListNode(1)
+    headA.next = ListNode(2)
+    headA.next.next = ListNode(3)
+    headA.next.next.next = ListNode(4)
+    headA.next.next.next.next = ListNode(5)
+    headB = ListNode(6)
+    headB.next = ListNode(7)
+    headB.next.next = ListNode(8)
+    headB.next.next.next = ListNode(9)
+    headB.next.next.next.next = ListNode(10)
+    print(solution.getIntersectionNode(headA, headB))
+>>>>>>> e2ded62 (update)
