bc6c4dfd7583d7eec0b70046d3344543643c1e20
qidong.xu <qidong.xu@tcredit.com>
2025-10-27 23:09:31 +0800

    优化最后计算平均值，前面只计算最大和即可

--- python-lab/slidingwindow/643.MaximumAverageSubarrayI.py
+++ python-lab/slidingwindow/643.MaximumAverageSubarrayI.py
@@ -27,6 +27,21 @@ class Solution:
 
         return max_average
 
+    def findMaxAverage1(self, nums: List[int], k: int) -> float:
+        length = len(nums)
+        max_sum = float('-inf')
+        current_sum = 0
+        for i in range(k):
+            current_sum += nums[i]
+        # 前 k 个元素的平均值
+        max_sum = current_sum
+
+        for i in range(k, length):
+            current_sum += nums[i] - nums[i - k]
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum / k
+
 if __name__ == "__main__":
     solution = Solution()
     nums = [1, 12, -5, -6, 50, 3]
