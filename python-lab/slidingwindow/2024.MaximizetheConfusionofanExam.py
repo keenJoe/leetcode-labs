f73fe05ad1f5bcaa810ed125287a06f783642ae4
qidongxu <qidongxuca@gmail.com>
2025-12-03 10:32:38 +0800

    最大的考试难度

---
+++ python-lab/slidingwindow/2024.MaximizetheConfusionofanExam.py
@@ -0,0 +1,73 @@
+# 2024. Maximize the Confusion of an Exam
+
+'''
+一位老师正在出一场由 n 道判断题构成的考试，每道题的答案为 true （用 'T' 表示）或者 false （用 'F' 表示）。
+老师想增加学生对自己做出答案的不确定性，方法是 最大化 有 连续相同 结果的题数。（也就是连续出现 true 或者连续出现 false）。
+
+给你一个字符串 answerKey ，其中 answerKey[i] 是第 i 个问题的正确结果。除此以外，还给你一个整数 k ，表示你能进行以下操作的最多次数：
+
+每次操作中，将问题的正确答案改为 'T' 或者 'F' （也就是将 answerKey[i] 改为 'T' 或者 'F' ）。
+请你返回在不超过 k 次操作的情况下，最大 连续 'T' 或者 'F' 的数目。
+
+ 
+
+示例 1：
+
+输入：answerKey = "TTFF", k = 2
+输出：4
+解释：我们可以将两个 'F' 都变为 'T' ，得到 answerKey = "TTTT" 。
+总共有四个连续的 'T' 。
+示例 2：
+
+输入：answerKey = "TFFT", k = 1
+输出：3
+解释：我们可以将最前面的 'T' 换成 'F' ，得到 answerKey = "FFFT" 。
+或者，我们可以将第二个 'T' 换成 'F' ，得到 answerKey = "TFFF" 。
+两种情况下，都有三个连续的 'F' 。
+示例 3：
+
+输入：answerKey = "TTFTTFTT", k = 1
+输出：5
+解释：我们可以将第一个 'F' 换成 'T' ，得到 answerKey = "TTTTTFTT" 。
+或者我们可以将第二个 'F' 换成 'T' ，得到 answerKey = "TTFTTTTT" 。
+两种情况下，都有五个连续的 'T' 。
+
+'''
+
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        max_length = 0
+        left = 0
+        count_F = 0
+        count_T = 0
+
+        for right in range(len(answerKey)):
+            if answerKey[right] == 'F':
+                count_F += 1
+
+            while count_F > k:
+                if answerKey[left] == 'F':
+                    count_F -= 1
+                left += 1
+
+            max_length = max(max_length, right - left + 1)
+
+        left = 0
+        for right in range(len(answerKey)):
+            if answerKey[right] == 'T':
+                count_T += 1
+
+            while count_T > k:
+                if answerKey[left] == 'T':
+                    count_T -= 1
+                left += 1
+
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
+
+if __name__ == "__main__":
+    solution = Solution()
+    answerKey = "TTFF"
+    k = 2
+    print(solution.maxConsecutiveAnswers(answerKey, k))
