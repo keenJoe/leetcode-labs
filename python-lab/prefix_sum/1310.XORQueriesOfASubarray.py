7e45a656534428e485a3dcba17648d5134260da5
qidongxu <qidongxuca@gmail.com>
2026-01-15 10:23:22 +0800

    异或运算的前缀和，注意异或运算的特点，相同项会被抵消

---
+++ python-lab/prefix_sum/1310.XORQueriesOfASubarray.py
@@ -0,0 +1,56 @@
+# 1310. XOR Queries of a Subarray
+
+
+'''
+有一个正整数数组 arr，现给你一个对应的查询数组 queries，其中 queries[i] = [Li, Ri]。
+
+对于每个查询 i，请你计算从 Li 到 Ri 的 XOR 值（即 arr[Li] xor arr[Li+1] xor ... xor arr[Ri]）作为本次查询的结果。
+
+并返回一个包含给定查询 queries 所有结果的数组。
+
+示例 1：
+输入：arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+输出：[2,7,14,8] 
+解释：
+数组中元素的二进制表示形式是：
+1 = 0001 
+3 = 0011 
+4 = 0100 
+8 = 1000 
+查询的 XOR 值为：
+[0,1] = 1 xor 3 = 2 
+[1,2] = 3 xor 4 = 7 
+[0,3] = 1 xor 3 xor 4 xor 8 = 14 
+[3,3] = 8
+
+示例 2：
+输入：arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+输出：[8,0,4,4]
+'''
+
+
+from typing import List
+
+
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        n = len(arr)
+
+        # 计算前缀异或数组
+        prefix_xor = [0] * (n + 1)
+        for i in range(n):
+            prefix_xor[i + 1] = prefix_xor[i] ^ arr[i]
+
+        result  = []
+        for L, R in queries:
+            # 异或运算特点：a ^ a = 0, a ^ 0 = a，即相同项会抵消
+            xor_value = prefix_xor[R + 1] ^ prefix_xor[L]
+            result.append(xor_value)
+
+        return result
+
+if __name__ == "__main__":
+    solution = Solution()
+    arr = [1, 3, 4, 8]
+    queries = [[0, 1], [1, 2], [0, 3], [3, 3]]
+    print(solution.xorQueries(arr, queries))
